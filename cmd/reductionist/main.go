bd9cca5466b4837ef0255596feac5d199b6bc9f2
Mark Goddard <markgoddard86@gmail.com>
2024-09-27 11:47:36 +0100

    Wait for workers to exit with a WaitGroup

--- cmd/reductionist/main.go
+++ cmd/reductionist/main.go
@@ -8,14 +8,32 @@ import (
 	"github.com/markgoddard/reductionist/pkg/handlers"
 	"github.com/markgoddard/reductionist/pkg/operations"
 	"github.com/markgoddard/reductionist/pkg/worker"
+
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	conf := config.Parse()
 	pool := worker.NewPool(10)
-	http.Handle("/v1/max", handlers.New(operations.Max{}, &pool))
-	http.Handle("/v1/min", handlers.New(operations.Min{}, &pool))
-	http.Handle("/v1/sum", handlers.New(operations.Sum{}, &pool))
+	handleShutdown(pool)
+	http.Handle("/v1/max", handlers.New(operations.Max{}, pool))
+	http.Handle("/v1/min", handlers.New(operations.Min{}, pool))
+	http.Handle("/v1/sum", handlers.New(operations.Sum{}, pool))
 	addr := fmt.Sprintf(":%d", conf.Port)
 	http.ListenAndServe(addr, nil)
+}
+
+func handleShutdown(pool *worker.Pool) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("Caught Ctrl-C")
+		pool.Close()
+		fmt.Println("Waiting for workers to exit")
+		pool.Join()
+		os.Exit(1)
+	}()
 }
--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -3,12 +3,15 @@ package worker
 import (
 	"fmt"
 
+	"sync"
+
 	"github.com/markgoddard/reductionist/pkg/operations"
 	"github.com/markgoddard/reductionist/pkg/request"
 )
 
 type Pool struct {
 	queue chan *Job
+	wg    sync.WaitGroup
 }
 
 type Job struct {
@@ -23,28 +26,38 @@ type result struct {
 	err  error
 }
 
-func NewPool(workers uint) Pool {
-	pool := Pool{make(chan *Job, workers)}
+func NewPool(workers uint) *Pool {
+	pool := Pool{queue: make(chan *Job, workers)}
 	for id := range workers {
 		go pool.worker(id)
 	}
-	return pool
+	pool.wg.Add(int(workers))
+	return &pool
 }
 
 func (p *Pool) Execute(job *Job) {
 	p.queue <- job
 }
 
+func (p *Pool) Close() {
+	close(p.queue)
+}
+
+func (p *Pool) Join() {
+	p.wg.Wait()
+}
+
 func (p *Pool) worker(id uint) {
 	// TODO: Handle shutdown.
 	fmt.Println("Worker", id, "starting")
-	for {
-		job := <-p.queue
+	for job := range p.queue {
 		fmt.Println("Worker", id, "received job")
 		data, err := job.operation.Execute(job.data, job.request_data)
 		fmt.Println("Worker", id, "completed job")
 		job.result <- result{data: data, err: err}
 	}
+	p.wg.Done()
+	fmt.Println("Worker", id, "exiting")
 }
 
 func NewJob(operation operations.Operation, data []byte, request_data request.Data) Job {
